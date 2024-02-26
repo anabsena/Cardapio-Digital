aa26d6dab136bd49f7a0a1d5eb831290c5bef2eb
guilherme <guilhermeg.dev@gmail.com>
2024-02-25 21:27:46 -0300

    review modal

---
+++ Views/Review/Review.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+export default function Review() {
+
+    return (
+        <View style={styles.title}>
+            <Text style={styles.titleText}>Review</Text>
+        </View>
+    )
+}
